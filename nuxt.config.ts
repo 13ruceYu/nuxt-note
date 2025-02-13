11fd619505c9078ec7f38cb641684ae137a10528
13ruceYu <bruceyuhongbo@gmail.com>
2025-02-13 09:50:56 +0800

    init

---
+++ nuxt.config.ts
@@ -0,0 +1,25 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  modules: ['@nuxt/ui'],
+  app: {
+    head: {
+      charset: 'utf-8',
+      title: 'Nuxt Note',
+      meta: [
+        {
+          name: 'description',
+          content: 'Nuxt Note'
+        }
+      ],
+      link: [
+        {
+          rel: 'icon',
+          type: 'image/x-icon',
+          href: '/logo.png'
+        }
+      ]
+    }
+  },
+})
