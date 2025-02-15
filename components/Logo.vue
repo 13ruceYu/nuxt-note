6a8a47dfdadf36cc7d4789e11798df27feea35d3
13ruceYu <bruceyuhongbo@gmail.com>
2025-02-15 10:36:57 +0800

    feat: note edit page ui

---
+++ components/Logo.vue
@@ -0,0 +1,5 @@
+<template>
+  <h1 class="text-xl flex">
+    <UIcon class="size-6 mr-1 text-primary" name="material-symbols:edit-square"></UIcon> Nuxt Note
+  </h1>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -12,8 +12,40 @@ const handleSignOut = async () => {
 </script>
 
 <template>
-  <div>
-    <u-button @click="handleSignOut">Sign out</u-button>
+  <div class="flex h-screen">
+    <div class="bg-black/50 w-[20rem] shrink-0 p-4">
+      <Logo class="mb-4"></Logo>
+      <div>
+        <div class="mb-2">Today</div>
+        <div>
+          <div class="px-4 py-3 rounded-lg hover:bg-primary/10 cursor-pointer" v-for="i in 3" :key="i">
+            <div>Finally moved into my</div>
+            <div class="text-sm flex">
+              <div class="mr-4">Today</div>
+              <div class="opacity-50 truncate">Life is like riding a bicycle. To keep your balance, you must keep
+                moving.</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="flex-grow">
+      <div class="flex justify-between items-center p-4">
+        <UButton color="gray" variant="ghost" icon="carbon:document-add">Create Note
+        </UButton>
+        <u-button color="gray" variant="ghost" icon="carbon:trash-can"></u-button>
+      </div>
+      <div class="p-6 flex justify-center">
+        <div>
+          <div class="h-10 text-gray-600">Feb 15, 2025</div>
+          <div class="w-[30rem]">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam culpa vero ipsam quia tempore animi
+            excepturi
+            eaque distinctio quis delectus aliquid eum placeat facilis id, maxime explicabo molestiae ut esse.
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
-  <h1 class="text-red-500">hi</h1>
+
 </template>
