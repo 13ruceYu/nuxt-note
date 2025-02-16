02131692162b1f555f7b08ada3770677a3be46be
13ruceYu <bruceyuhongbo@gmail.com>
2025-02-16 17:52:08 +0800

    fix: nuxt production .prisma path error

--- types/note.ts
+++ types/note.ts
@@ -1,5 +1,12 @@
-import type { Note } from '@prisma/client'
+export interface Note {
+  id: number
+  title: string
+  text: string
+  updatedAt: Date
+  createdAt: Date
+  userId: number
+}
 
 export interface ExtendedNote extends Note {
-  updatedDate: string;
+  updatedDate: string
 }
